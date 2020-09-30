87667ffdd807b0a50dff39f72452c1c178bbcd74
Victor Louis Carles <victor.carles92@gmail.com>
2020-09-30 15:28:05 +0200

    init commit

---
+++ pi.c
@@ -0,0 +1,55 @@
+/*
+============================================================================
+Filename    : pi.c
+Author      : Victor Carles
+SCIPER		: 289407
+============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utility.h"
+
+double calculate_pi (int num_threads, int samples);
+
+int main (int argc, const char *argv[]) {
+
+    int num_threads, num_samples;
+    double pi;
+
+    if (argc != 3) {
+		printf("Invalid input! Usage: ./pi <num_threads> <num_samples> \n");
+		return 1;
+	} else {
+        num_threads = atoi(argv[1]);
+        num_samples = atoi(argv[2]);
+	}
+
+    set_clock();
+    pi = calculate_pi (num_threads, num_samples);
+
+    printf("- Using %d threads: pi = %.15g computed in %.4gs.\n", num_threads, pi, elapsed_time());
+
+    return 0;
+}
+
+
+double calculate_pi (int num_threads, int samples) {
+    double pi;
+
+    omp_set_num_threads(num_threads);
+
+    unsigned int nber_points = 0;
+    rand_gen seed = init_rand();
+
+    #pragma omp parallel 
+    for (size_t i = 0; i < samples; i++) {
+        double x = next_rand(seed);
+        double y = next_rand(seed);
+
+        if (x*x + y*y <= 1.0) nber_points++;        
+    }
+
+    pi = ((double)nber_points/(double)samples) * 4.0;
+    return pi;
+}
